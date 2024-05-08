7faf9db9608d1168a9db5c5023085c1c2fc0f1b9
Beez <vacuaspab@utn.edu.ec>
2024-05-08 09:21:53 -0500

    Agregar archivos de proyecto.

---
+++ MauiApiConsumer/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace MauiApiConsumer
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new Main();
+        }
+    }
+}
---
+++ MauiApiConsumer/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace MauiApiConsumer
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
